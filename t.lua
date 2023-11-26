08fb2487016c7ca3f194d77e98c0c5191a68ab70
MeowKJ <ijink@icloud.com>
2023-11-26 16:47:25 +0800

    2

--- t.lua
+++ t.lua
@@ -56,8 +56,6 @@ local function main()
             checkFuel()
             chopTree()
             collectWood()
-
-            x = x + 1
             if x > 6 then
                 if x % 2 == 0 then
                     turtle.turnRight()
@@ -72,6 +70,7 @@ local function main()
                 x = 0
                 y = y + 1
             end
+            x = x + 1
         end
         goHome()
         x = 0
