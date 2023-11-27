178a0039e3d724a7b978211fec367437d4fa5034
MeowKJ <ijink@icloud.com>
2023-11-27 14:05:21 +0800

    suckup

--- tree1.lua
+++ tree1.lua
@@ -55,7 +55,7 @@ local function chopTreeAndplaceSapling(isLeft)
     if not turtle.detect() then
         -- suck fornt
         turtle.suck()
-        turtle.suckup()
+        turtle.suckUp()
 
         -- suck left
         turtle.turnLeft()
